a9689a87a40c62c80933526dc7de6aa746615dc4
Andrey <andrey.cortez12@gmail.com>
2023-10-13 17:56:20 -0300

    Mudanças irrelevantes

--- improc/train.py
+++ improc/train.py
@@ -3,7 +3,7 @@ import os
 
 # Load a model
 # Load a model
-model = YOLO('model/yolov8n.yaml')  # build a new model from YAML
+model = YOLO('model/yolov8n.pt')  # build a new model from YAML
 
 # Train the model
-results = model.train(data= os.getcwd() + '/datasets/cones/data.yaml', epochs=2, imgsz=255)
+results = model.train(data= os.getcwd() + '/datasets/fsoco_yolov8/data.yaml', epochs=10, imgsz=255)
